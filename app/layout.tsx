a692dd2da6aafcdf56e2454f62f154af4991228b
raahimanowar <raahimfardin@gmail.com>
2024-09-21 05:52:08 +0600

    Added vercel analytics in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -44,6 +45,7 @@ export default function RootLayout({
               {children}
             </main>
           </div>
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
